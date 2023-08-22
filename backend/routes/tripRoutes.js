02d65894f39775653ce1b93dfb0c14effede9acb
prakharagarwal <prakharagarwal3031@gmail.com>
2023-08-23 00:52:21 +0530

    feat: connect mongodb, update controllers and routes
    Signed-off-by: prakharagarwal <prakharagarwal3031@gmail.com>

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const tripController = require("../controllers/tripController");
 const passport = require("passport");
 
-// Get all trips
-router.get("/", tripController.getTrips);
-
-// Get a trip by ID
-router.get("/:id", tripController.getTripById);
-
 // Create a new trip
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   tripController.createTrip
 );
+
+// Get all trips
+router.get("/", tripController.getTrips);
+
+// Get a trip by ID
+router.get("/:id", tripController.getTripById);
 
 // Update a trip
 router.put(
@@ -28,6 +28,51 @@ router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   tripController.deleteTrip
+);
+
+// Join a trip
+router.post(
+  "/:id/join",
+  passport.authenticate("jwt", { session: false }),
+  tripController.joinTrip
+);
+
+// Get members of a trip
+router.get("/:id/members", tripController.getTripMembers);
+
+// Get trips of a user
+router.get(
+  "/user/:userId",
+  passport.authenticate("jwt", { session: false }),
+  tripController.getUserTrips
+);
+
+// Remove user from a trip
+router.delete(
+  "/:id/leave",
+  passport.authenticate("jwt", { session: false }),
+  tripController.leaveTrip
+);
+
+// Send a join request to a trip
+router.post(
+  "/:id/join-request",
+  passport.authenticate("jwt", { session: false }),
+  tripController.joinTrip
+);
+
+// Accept a join request for a trip
+router.put(
+  "/:id/accept-request/:userId",
+  passport.authenticate("jwt", { session: false }),
+  tripController.acceptJoinRequest
+);
+
+// Get pending join requests for a trip
+router.get(
+  "/:id/pending-requests",
+  passport.authenticate("jwt", { session: false }),
+  tripController.getPendingJoinRequests
 );
 
 module.exports = router;
