39ffe3ed383a32f6c74e53b5dc1c262e0b56287d
prakharagarwal <prakharagarwal3031@gmail.com>
2023-08-26 12:48:23 +0530

    feat: integrate backend
    Signed-off-by: prakharagarwal <prakharagarwal3031@gmail.com>

--- app/app/trip/index.js
+++ app/app/trip/index.js
@@ -2,55 +2,64 @@ import { useState } from "react";
 import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
-import { COLORS, icons, images, SIZES } from '../../constants'
-import { ScreenHeaderBtn, Welcome, TripList } from '../../components'
+import { COLORS, icons, images, SIZES } from "../../constants";
+import { ScreenHeaderBtn, Welcome, TripList } from "../../components";
 import CreateButton from "../../components/common/button/create/CreateButton";
+import { axiosInstance } from "../../config/api";
 
 const Trip = () => {
-    const router = useRouter()
-    const [searchTerm, setSearchTerm] = useState("")
-    const activeTab = "Trip"
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <Stack.Screen
-                options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite },
-                    headerShadowVisible: false,
-                    headerLeft: () => (
-                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-                    ),
-                    headerRight: () => (
-                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
-                    ),
-                    headerTitle: ""
-                }} />
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View
-                    style={{
-                        flex: 1,
-                        padding: SIZES.medium
-                    }}
-                >
+  const router = useRouter();
+  const [trips, setTrips] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const activeTab = "Trip";
 
-                    <Welcome
-                        welcomeMessage={"Trips around you"}
-                        isHomePage={false}
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                        handleClick={() => {
-                            if (searchTerm) {
-                                router.push(`/trip/${searchTerm}`)
-                            }
-                        }}
-                        activeTab={activeTab}
-                    />
+  useEffect(async () => {
+    // Fetch trip data using Axios
+    const response = await axiosInstance.get("/trip");
+    setTrips(response.data);
+  }, []);
 
-                    <TripList />
-                </View>
-            </ScrollView>
-            <CreateButton activeTab={activeTab} />
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          // headerLeft: () => (
+          //     <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          // ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome
+            welcomeMessage={"Trips for you"}
+            isHomePage={false}
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/trip/${searchTerm}`);
+              }
+            }}
+            activeTab={activeTab}
+          />
 
-export default Trip
+          <TripList />
+        </View>
+      </ScrollView>
+      <CreateButton activeTab={activeTab} />
+    </SafeAreaView>
+  );
+};
+
+export default Trip;
--- app/components/common/welcome/Welcome.jsx
+++ app/components/common/welcome/Welcome.jsx
@@ -1,56 +1,71 @@
-import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
-import { useRouter } from 'expo-router'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
-import { icons } from '../../../constants'
+import styles from "./welcome.style";
+import { icons } from "../../../constants";
 
-const activeTabs = ["Community", "Trip"]
+const activeTabs = ["Community", "Trip"];
 
-const Welcome = ({ welcomeMessage, isHomePage, searchTerm, setSearchTerm, handleClick, activeTab }) => {
-    const router = useRouter()
+const Welcome = ({
+  welcomeMessage,
+  isHomePage,
+  searchTerm,
+  setSearchTerm,
+  handleClick,
+  activeTab,
+}) => {
+  const router = useRouter();
 
-    return (
-        <View>
+  return (
+    <View>
+      <View style={styles.container}>
+        {isHomePage && <Text style={styles.userName}>Hello Adi</Text>}
+        <Text style={styles.welcomeMessage}>{welcomeMessage}</Text>
+      </View>
 
-            <View style={styles.container}>
-                {isHomePage && <Text style={styles.userName}>Hello Adi</Text>}
-                <Text style={styles.welcomeMessage}>{welcomeMessage}</Text>
-            </View>
-
-            {!isHomePage && <View style={styles.searchContainer}>
-                <View style={styles.searchWrapper}>
-                    <TextInput
-                        style={styles.searchInput}
-                        value={searchTerm}
-                        onChangeText={(text) => setSearchTerm(text)}
-                        placeholder={`Search for ${activeTab}`}
-                    />
-                </View>
-                <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
-                    <Image
-                        source={icons.search}
-                        resizeMode='contain'
-                        style={styles.searchBtnImage}
-                    />
-                </TouchableOpacity>
-            </View>}
-
-            <View style={styles.tabsContainer}>
-                {activeTabs.map(item => (
-                    <View style={styles.tabsWrapper} >
-                        <TouchableOpacity
-                            style={styles.tab(activeTab, item)}
-                            onPress={() => {
-                                router.push(`/${item.toLowerCase()}`)
-                            }}
-                        >
-                            <Text style={styles.tabText(activeTab, item)}>{item}</Text>
-                        </TouchableOpacity>
-                    </View>
-                ))}
-            </View>
+      {!isHomePage && (
+        <View style={styles.searchContainer}>
+          <View style={styles.searchWrapper}>
+            <TextInput
+              style={styles.searchInput}
+              value={searchTerm}
+              onChangeText={(text) => setSearchTerm(text)}
+              placeholder={`Search for ${activeTab}`}
+            />
+          </View>
+          <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
+            <Image
+              source={icons.search}
+              resizeMode="contain"
+              style={styles.searchBtnImage}
+            />
+          </TouchableOpacity>
         </View>
-    )
-}
+      )}
 
-export default Welcome
+      <View style={styles.tabsContainer}>
+        {activeTabs.map((item) => (
+          <View style={styles.tabsWrapper}>
+            <TouchableOpacity
+              style={styles.tab(activeTab, item)}
+              onPress={() => {
+                router.replace(`/${item.toLowerCase()}`);
+              }}
+            >
+              <Text style={styles.tabText(activeTab, item)}>{item}</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
