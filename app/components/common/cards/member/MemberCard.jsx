9e71423f738b1e53f0da173ab77860cd56ecb1c1
menacingsoul <adarsh14103@gmail.com>
2023-08-31 01:41:08 +0530

    expo-removed

--- app/components/common/cards/member/MemberCard.jsx
+++ app/components/common/cards/member/MemberCard.jsx
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './member.style';
-import { images } from '../../../../constants';
+
 
 const MemberCard = ({ member, handleNavigate }) => {
     return (
